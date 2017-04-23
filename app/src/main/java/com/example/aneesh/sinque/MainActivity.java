a5e284a2ee9e98ad96614bb306d1a2e90b7c653a
aneeshverenkar <aneesh.verenkar@gmail.com>
2017-04-23 09:33:59 -0700

    Get requests

--- app/src/main/java/com/example/aneesh/sinque/MainActivity.java
+++ app/src/main/java/com/example/aneesh/sinque/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.aneesh.sinque;
 
+import android.graphics.Typeface;
 import android.media.MediaRecorder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,14 +19,25 @@ import com.ibm.watson.developer_cloud.text_to_speech.v1.model.AudioFormat;
 
 import com.ibm.watson.developer_cloud.android.library.audio.MicrophoneInputStream;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     MicrophoneInputStream capture;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        /*Button myTextView = (Button) findViewById(R.id.button1);
+        Typeface typeface= Typeface.createFromAsset(getAssets(), "fonts/Moon Light.otf");
+        myTextView.setTypeface(typeface);*/
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int sampleRate = 16000;
@@ -37,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
                 capture = new MicrophoneInputStream(true);
                 SpeechToText service = new SpeechToText();
                 service.setUsernameAndPassword("f7c43500-f215-48c5-8697-9f288814ea0e", "VJ00zCxcI8y0");
+
+                Log.d("Print","clicked");
 
 
                 try {
@@ -64,13 +78,88 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onTranscription(SpeechResults speechResults) {
-            System.out.println(speechResults);
-            if(speechResults.getResults() != null && !speechResults.getResults().isEmpty()) {
+            //System.out.println(speechResults);
+            if (speechResults.getResults() != null && !speechResults.getResults().isEmpty()) {
                 String text = speechResults.getResults().get(0).getAlternatives().get(0).getTranscript();
                 //showMicText(text);
-                Log.d("print",text);
+
+                if (text.contains("pause")) {
+                    URL url;
+                    HttpURLConnection urlConnection = null;
+                    Log.d("Print","Asd");
+                    try {
+                        url = new URL("http://5ec67a0f.ngrok.io/sampleurl?name=pause");
+
+                        urlConnection = (HttpURLConnection) url
+                                .openConnection();
+
+                        InputStream in = urlConnection.getInputStream();
+
+
+
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (urlConnection != null) {
+                            urlConnection.disconnect();
+                        }
+                    }
+
+
+                }else if (text.contains("play")){
+                    URL url;
+                    HttpURLConnection urlConnection = null;
+                    Log.d("Print","Asd");
+                    try {
+                        url = new URL("http://5ec67a0f.ngrok.io/sampleurl?name=play");
+
+                        urlConnection = (HttpURLConnection) url
+                                .openConnection();
+
+                        InputStream in = urlConnection.getInputStream();
+
+
+
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (urlConnection != null) {
+                            urlConnection.disconnect();
+                        }
+                    }
+                }else if(text.contains("next")){
+                    URL url;
+                    HttpURLConnection urlConnection = null;
+                    Log.d("Print","Asd");
+                    try {
+                        url = new URL("http://5ec67a0f.ngrok.io/sampleurl?name=next");
+
+                        urlConnection = (HttpURLConnection) url
+                                .openConnection();
+
+                        InputStream in = urlConnection.getInputStream();
+
+
+
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (urlConnection != null) {
+                            urlConnection.disconnect();
+                        }
+                    }
+                }
+
+
             }
         }
+
 
         @Override public void onConnected() {
 
@@ -80,7 +169,7 @@ public class MainActivity extends AppCompatActivity {
             //showError(e);
             //enableMicButton();
         }
-
+;
         @Override public void onDisconnected() {
             //enableMicButton();
         }
